ab33839fb50c5d50dcd450120baf21a09d02f702
User <User@DESKTOP-E0L91G2>
2024-01-17 23:21:04 +0200

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Nume_Pren_Lab2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
